e7b0a24c5e088fbc2c627512449aae884074c811
Bryce Walsh <bryce.j.walsh@gmail.com>
2020-04-21 15:15:41 -0400

    Added per capita functionality

--- Emissions_Data/CountryCodeScript.py
+++ Emissions_Data/CountryCodeScript.py
@@ -3,4 +3,4 @@ import country_converter as coco
 df = pd.read_csv('historical_emissions.csv')
 
 df['ISO3'] = df['Country'].apply(lambda x: coco.convert(names=x, to='ISO3'))
-df[df.ISO3 != 'not found'].to_csv('Emissions_with_ISO3_unabridged.csv')
+df[df.ISO3 != 'not found'].to_csv('Emissions_with_ISO3.csv')
