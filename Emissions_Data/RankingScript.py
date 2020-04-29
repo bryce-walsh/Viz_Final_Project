e4544f6f54ca270c1d06d9776c17cb9fb3d78f4d
Bryce Walsh <bryce.j.walsh@gmail.com>
2020-04-29 13:56:10 -0400

    switch emissions rankings

--- Emissions_Data/RankingScript.py
+++ Emissions_Data/RankingScript.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import country_converter as coco
-pd = pd.read_csv('emissions_per_cap_no_nans.csv')
+pd = pd.read_csv('Emissions_with_ISO3.csv')
 pd = pd.set_index('ISO3')
-pd = pd.rank().reset_index()
-pd.to_csv('emissions_per_cap_rankings.csv')
+pd = pd.rank(ascending = False).reset_index()
+pd.to_csv('emissions_rankings.csv')
